91799388519914bbd71cbbbaa1f75fbbd4f789ab
Jonathan Skårstedt <nyson@jotaro>
2020-05-03 18:47:40 +0200

    Added shorthand for generating json

--- Data/MakeObj.hs
+++ Data/MakeObj.hs
@@ -6,6 +6,7 @@ module Data.MakeObj
   , pp, pprint, jsonStructure, jsonStructureEquality
   , generateObj, generateList, mkTypeLabel, genTree
   , tryParser
+  , generateJson
   ) where
 
 import Data.MakeObj.AST (
@@ -24,6 +25,18 @@ import Data.MakeObj.PP (PP(..), pprint)
 import Data.MakeObj.Parser hiding (rx)
 import Data.MakeObj.Parser.Shared (Error, Parser)
 import Data.MakeObj.TreeEquality (jsonStructure, jsonStructureEquality)
+
+import Data.Aeson (Value)
+import Test.QuickCheck (generate)
+import Text.Megaparsec.Error(errorBundlePretty)
+
+
+generateJson :: String -> String -> IO (Either String Value)
+generateJson typeInput defsInput = case parseGenerateTree typeInput of
+  Left err -> return $ Left ("Bad structure parse:\n" ++ errorBundlePretty err)
+  Right parsedJsonType -> case parseDefs defsInput of
+    Left err -> return $ Left ("Bad definitions parse:\n" ++ errorBundlePretty err)
+    Right definitions -> Right <$> generate (generateObj definitions parsedJsonType)
 
 tryParser :: (PP t, Show t) => Parser t -> String -> IO ()
 tryParser p input = case parse p "stdin" input of
