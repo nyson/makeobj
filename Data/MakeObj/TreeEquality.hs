1cccf29ce676daf4d965004d89b758da16d30b18
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2022-12-20 09:05:54 +0100

    Maintenance/update packages (#29)
    * added dev container
    
    * updated packages and fixed some breaking changes
    
    * added extensions and issue tracker
    
    Co-authored-by: Jonathan Skårstedt <jonathan.skarstedt@ecarxgroup.com>

--- Data/MakeObj/TreeEquality.hs
+++ Data/MakeObj/TreeEquality.hs
@@ -2,13 +2,19 @@
 module Data.MakeObj.TreeEquality where
 
 import Data.Aeson (ToJSON, FromJSON(parseJSON), Value(..), decode, encode)
+import Data.Aeson.KeyMap (KeyMap)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text, unpack)
-import qualified Data.HashMap.Strict as Map
+import qualified Data.HashMap.Strict as HM
 import Data.HashMap.Strict (HashMap)
 import Data.MakeObj.PP
 
 import qualified Data.Vector as V
+import qualified Data.Aeson.KeyMap as KM
+import qualified Data.Aeson.Key as Key
+
+keyMapToTextHashMap :: KeyMap a -> HashMap Text a
+keyMapToTextHashMap = HM.mapKeys (Key.toText) . KM.toHashMap
 
 data ValType = TString | TNumber | TBool | TNull
   deriving (Eq, Show)
@@ -32,7 +38,7 @@ instance PP JSONStructure where
           ppKV key val = replicate (il * 2) ' '
                          ++ unpack key ++ ": "
                          ++ ppt il val ++ "\n"
-          elems = concatMap snd . Map.toList $ Map.mapWithKey ppKV hm
+          elems = concatMap snd . HM.toList $ HM.mapWithKey ppKV hm
       in "{\n" ++ elems ++ replicate (i * 2) ' ' ++ "} "
 
 instance FromJSON JSONStructure where
@@ -42,7 +48,7 @@ instance FromJSON JSONStructure where
     Bool   _ -> return $ TNode TBool
     Null     -> return $ TNode TNull
     Array arr -> TList <$> mapM parseJSON (V.toList arr)
-    Object o -> TObject <$> sequence (Map.map parseJSON o)
+    Object o -> TObject <$> sequence (HM.map parseJSON $ keyMapToTextHashMap o)
 
 jsonStructure :: ToJSON a => a -> Maybe JSONStructure
 jsonStructure = decode . encode
